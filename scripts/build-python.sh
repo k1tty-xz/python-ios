c81bab8a85d89d99527ea97ba5b28c18a6eaa0a4
k1tty-xz <187893856+k1tty-xz@users.noreply.github.com>
2026-09-02 09:43:52 +0200

    Disable unsupported IPv6 configure probe

--- scripts/build-python.sh
+++ scripts/build-python.sh
@@ -41,6 +41,7 @@ tar -xzf "$archive" -C "$BUILD"
 		--with-system-libmpdec=no \
 		--with-ensurepip=no \
 		--disable-test-modules \
+		--disable-ipv6 \
 		LIBFFI_CFLAGS="$LIBFFI_CFLAGS" \
 		LIBFFI_LIBS="$LIBFFI_LIBS"
 	make -j"$JOBS"
